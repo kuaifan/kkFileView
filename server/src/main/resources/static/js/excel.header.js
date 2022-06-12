f41f61211b1a9739224e1a5cbb7e5e20a50d68ee
kuaifan <aipaw@live.cn>
2022-06-12 15:32:20 +0800

    perf: 优化excel导航条

--- server/src/main/resources/static/js/excel.header.js
+++ server/src/main/resources/static/js/excel.header.js
@@ -1,7 +1,22 @@
 /**创建一个div并固定在底部，将center中的所有a标签放在改div中**/
-$("body").append($("<div>").css({"width":"100%","max-height":"93px","position":"sticky","bottom":"-1px","left":"0"
-    ,"background-color":"rgba(53, 53, 53, 1)","line-height":"30px","font-size":"13px","overflow":"auto","display":"flex","flex-wrap":"wrap"}).attr("id","excel-header-nav"));
-$("center").css("display", "none");
+$("body").append($("<div>").css({
+    "width": "100%",
+    "max-height": "92px",
+    "position": "sticky",
+    "left": "0",
+    "bottom": "0",
+    "background-color": "rgba(53, 53, 53, 1)",
+    "overflow": "auto"
+}).append($("<div>").css({
+    "width": "100%",
+    "line-height": "30px",
+    "font-size": "13px",
+    "overflow": "hidden",
+    "display": "flex",
+    "flex-wrap": "wrap",
+    "margin-top": "-1px",
+    "margin-left": "-1px"
+}).attr("id", "excel-header-nav")));
 var centerChildrenA = $("center").children("a");
 if (centerChildrenA.length === 0) {
     $("#excel-header-nav").hide();
@@ -10,13 +25,21 @@ $(centerChildrenA).each(function (a, b) {
     // 获取a标签对应的target的name值，并设置name对应标签的样式以避免锚点标签标题被覆盖
     var href = $(b).attr("href");
     var name = href.substr(1);
-    $("[name=" + name + "]").css({"display":"block","padding-top":"14.01px"});
-    $(b).css({"padding":"0 8px","border-right":"1px solid white","border-bottom":"1px solid white","color":"#f9f9f9"});
+    $("[name=" + name + "]").css({"display": "block"});
+    $(b).css({
+        "padding": "0 8px",
+        "border": "1px solid white",
+        "color": "#f9f9f9",
+        "margin-right": "-1px",
+        "margin-bottom": "-1px",
+    });
     $("#excel-header-nav").append(b);
 });
 /**给所有的table添加class=table table-striped样式**/
-$("table").addClass("table table-striped");
+$("table").addClass("table table-striped").css("width", "auto");
 /**去除顶部空白**/
-if ($("[name=table0]").length > 0) {
-    $("body").css("margin-top", $("[name=table0]")[0].offsetTop * -1)
+var prev = $("[name=table0]").prev();
+while (prev.length > 0) {
+    prev.hide();
+    prev = prev.prev();
 }
