7269fbfb57208e81ba6361f5963ec445a071fd30
Pang <aipaw@live.cn>
2022-06-13 08:16:34 +0800

    fix: 修复上一个提交的错误

--- server/src/main/java/cn/keking/service/FileHandlerService.java
+++ server/src/main/java/cn/keking/service/FileHandlerService.java
@@ -146,11 +146,17 @@ public class FileHandlerService {
                 if (line.contains("charset=gb2312")) {
                     line = line.replace("charset=gb2312", "charset=utf-8");
                 }
+                if (line.contains("</head>")) {
+                    sb.append("<meta id=\"viewport\" name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=2, minimum-scale=1, user-scalable=yes\">");
+                    sb.append("<link rel=\"stylesheet\" href=\"bootstrap/css/bootstrap.min.css\">");
+                    sb.append("<style>body{visibility:hidden}center{display:none}table{table-layout:fixed !important;margin-bottom:0 !important}</style>");
+                }
+                if (line.contains("</html>")) {
+                    sb.append("<script src=\"js/jquery-3.0.0.min.js\" type=\"text/javascript\"></script>");
+                    sb.append("<script src=\"js/excel.header.js\" type=\"text/javascript\"></script>");
+                }
                 sb.append(line);
             }
-            // 添加sheet控制头
-            sb = sb.replace("</head>", "<style>body{visibility:hidden}center{display:none}table{table-layout:fixed !important;margin-bottom:0 !important}</style><link rel=\"stylesheet\" href=\"bootstrap/css/bootstrap.min.css\"><meta id=\"viewport\" name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=2, minimum-scale=1, user-scalable=yes\"></head>");
-            sb = sb.replace("</html>", "<script src=\"js/jquery-3.0.0.min.js\" type=\"text/javascript\"></script><script src=\"js/excel.header.js\" type=\"text/javascript\"></script></html>");
         } catch (IOException e) {
             e.printStackTrace();
         }
